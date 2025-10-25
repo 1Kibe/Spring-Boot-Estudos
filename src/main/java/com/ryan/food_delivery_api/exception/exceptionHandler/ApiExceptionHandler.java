e85f60aabc03fddb8992d423953419e0483ef4b4
1Kibe <ryanadrianamendoin@gmail.com>
2025-10-25 15:43:06 -0300

    15

--- src/main/java/com/ryan/food_delivery_api/exception/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/ryan/food_delivery_api/exception/exceptionHandler/ApiExceptionHandler.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ryan.food_delivery_api.exception.EntidadeEmUsoException;
 import com.ryan.food_delivery_api.exception.EntidadeNaoEncontradaException;
 import com.ryan.food_delivery_api.exception.NegocioException;
 
 @ControllerAdvice // define que todas as exceptions de todos os controllers do projeto serao tratadas aqui
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 
     //customiza o body de resposta
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
@@ -44,4 +45,16 @@ public class ApiExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(layoutBodyy);
     }
+
+    //Customiza a resposta pradrao do ResponseEntityExceptionHandler
+    //@Override
+    //@Nullable
+    //protected ResponseEntity<Object> handleExceptionInternal(Exception arg0, @Nullable Object arg1, HttpHeaders arg2,
+    //        HttpStatusCode arg3, WebRequest arg4) {
+    //    arg1 = ModeloLayout.builder()
+    //        .dataHora(OffsetDateTime.now())
+    //        .mensagem(arg0.getMessage())
+    //        .build();
+    //    return super.handleExceptionInternal(arg0, arg1, arg2, arg3, arg4);
+    //}
 }
