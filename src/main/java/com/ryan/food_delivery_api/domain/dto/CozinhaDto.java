9e41824ab059c1175402363a3ff38e853549785e
1Kibe <ryanadrianamendoin@gmail.com>
2025-10-27 00:21:43 -0300

    39

---
+++ src/main/java/com/ryan/food_delivery_api/domain/dto/CozinhaDto.java
@@ -0,0 +1,11 @@
+package com.ryan.food_delivery_api.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CozinhaDto {
+    private Long id;
+    private String nome;
+}
---
+++ src/main/java/com/ryan/food_delivery_api/domain/dto/RestauranteDto.java
@@ -0,0 +1,16 @@
+package com.ryan.food_delivery_api.domain.dto;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestauranteDto {
+
+    private Long id;
+    private String nome;
+    private BigDecimal taxaFrete;
+    private CozinhaDto cozinha;
+}
