6df93e2d726957ee8c4043b248cbbbcbd25639ab
1Kibe <ryanadrianamendoin@gmail.com>
2025-10-28 00:20:57 -0300

    45

--- src/main/java/com/ryan/food_delivery_api/domain/dto/assembler/restaurante/RestauranteDtoDisassembler.java
+++ src/main/java/com/ryan/food_delivery_api/domain/dto/assembler/restaurante/RestauranteDtoDisassembler.java
@@ -3,6 +3,7 @@ package com.ryan.food_delivery_api.domain.dto.assembler.restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ryan.food_delivery_api.domain.Cozinha;
 import com.ryan.food_delivery_api.domain.Restaurante;
 import com.ryan.food_delivery_api.domain.dto.restaurante.RestauranteInputDto;
 
@@ -16,6 +17,8 @@ public class RestauranteDtoDisassembler {
     }
 
     public void copyToDomainObject(RestauranteInputDto inputDto, Restaurante entit){
+        //trata erro de auteracao 
+        entit.setCozinha(new Cozinha());
         modelMapper.map(inputDto, entit);
     }
 
